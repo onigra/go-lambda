570aebac1099e1b95b5c39400f5ebfb15ddfeb44
onigra <3280467rec@gmail.com>
2018-12-05 19:22:51 +0900

    eventを扱う

--- lambda/main.go
+++ lambda/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"github.com/aws/aws-sdk-go-v2/service/ssm"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/aws/endpoints"
 	"github.com/aws/aws-sdk-go-v2/aws/external"
-	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
 )
 
 // Handler :
-func Handler() (string, error) {
+func Handler(event events.CloudWatchEvent) (string, error) {
 	cfg, err := external.LoadDefaultAWSConfig()
 	if err != nil {
 		panic("unable to load SDK config, " + err.Error())
