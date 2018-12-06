15a592030a19991d6d25853e26078a333669c46c
onigra <3280467rec@gmail.com>
2018-12-06 11:38:03 +0900

    パラメータを受けて、Successだったらcurrent更新、それ以外だったらnewを消 すようにした

--- lambda/main.go
+++ lambda/main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"encoding/json"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/aws/endpoints"
 	"github.com/aws/aws-sdk-go-v2/aws/external"
 	"github.com/aws/aws-sdk-go-v2/service/ssm"
 )
+
+// ParseDetail :
+func ParseDetail(event events.CloudWatchEvent) map[string]interface{} {
+	var detail map[string]interface{}
+
+	err := json.Unmarshal(event.Detail, &detail)
+	if err != nil {
+		panic("detail Unmarshal error.")
+	}
+
+	return detail
+}
 
 // Handler :
 func Handler(event events.CloudWatchEvent) (string, error) {
@@ -18,8 +32,17 @@ func Handler(event events.CloudWatchEvent) (string, error) {
 	cfg.Region = endpoints.ApNortheast1RegionID
 	service := ssm.New(cfg)
 
-	result := UpdateParamStore(service)
-	return result, nil
+	detail := ParseDetail(event)
+	deployParams := detail["parameters"].(map[string]interface{})
+
+	if detail["status"] == "Success" {
+		newManifestPath := GetNewManifestPath(service, deployParams["newParam"].(string))
+		UpdateCurrentManifestPath(service, newManifestPath, deployParams["currentParam"].(string))
+	}
+
+	DeleteNewManifestParam(service, deployParams["newParam"].(string))
+
+	return "ok", nil
 }
 
 func main() {
